fd16cee152c14d62bca1315f4f1ec798f4ab9b85
TomatoKicker <phi465@live.de>
2023-05-18 00:25:37 +0200

    sizing

--- src/ContainerMeme.js
+++ src/ContainerMeme.js
@@ -8,7 +8,7 @@ function ContainerMeme() {
   return (
 	<Container>
 	  <Row className='d-flex justify-content-center'>
-	  	<Col xs={12} className={"d-flex justify-content-center"}>
+	  	<Col xs={12} md={6} className={"d-flex justify-content-center"}>
 	  		<CardMeme/>
 	  	</Col>
 	  </Row>
